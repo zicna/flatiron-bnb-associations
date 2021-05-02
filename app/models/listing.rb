b4039c5cfd68bd00d1dab84b736faaceb261fbf9
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-05-02 09:22:09 -0500

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,9 +2,10 @@ class Listing < ApplicationRecord
     belongs_to :host, :class_name => "User"
     # belongs_to :city
     belongs_to :neighborhood
+    
     has_many :reservations
     has_many :reviews, through: :reservations
-    
+
     has_many :guests, through: :reservations
     
 end
